1aa9bf26420c48bc2bf8c04053609a1af7e9d5d5
abnerjoy <2375559509@qq.com>
2022-07-03 20:03:15 +0800

    commit lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,16 @@
  */
 public class LeapYear {
 
+    /**
+    * leap yeat calculate
+    */
+    public static boolean isLeapYear(int year){
+        if(year % 400 == 0 ||(year % 4 ==0 && year % 100 != 0)){
+            return true;
+        }
+        return false;
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
