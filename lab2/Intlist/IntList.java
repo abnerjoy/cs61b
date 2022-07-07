7d0df88bb5eeac67c6e8bc72e5ef4046bb4df9ad
abnerjoy <2375559509@qq.com>
2022-07-07 17:54:20 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,19 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null) {
+            return B;
+        }
+        if(B == null){
+            return A;
+        }
+        //find the last node of A
+        IntList temp = A;
+        while (temp.rest != null){
+            temp = temp.rest;
+        }
+        temp.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +103,14 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null){
+            return B;
+        }
+        if(A.rest == null){
+            return new IntList(A.first,B);
+        }
+        return new IntList(A.first,catenate(A.rest,B));
+
     }
 
 
